939507150d17fcbe261c8cdec37ebb2efd88100e
Farah Manowar <saudiaa.xx@hotmail.com>
2021-05-05 23:54:06 -0400

    Adding PyPoll.py file

---
+++ PyPoll.py
@@ -0,0 +1,10 @@
+
+# Assign a variable for the file to load and the path.
+file_to_load = 'C:\Users\Farah\desktop\Resources\election_data.csv'
+
+
+# Open the election results and read the file
+with open(file_to_load) as election_data:
+
+     # To do: perform analysis.
+     print(election_data)
